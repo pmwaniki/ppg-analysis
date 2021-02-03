481d132496140eba233a83181426306978fefb3f
Paul Mwaniki <pmmwaniki06@gmail.com>
2021-02-03 10:34:58 +0300

    Logistic regression using l2 regularization and default solver

--- models/admission_classifier.py
+++ models/admission_classifier.py
@@ -43,7 +43,12 @@ admitted_test=np.stack(map(lambda id:test.loc[test['id']==id,'admitted'].iat[0],
 #                        # class_weight='balanced',
 #                        penalty='l2',
 #                        early_stopping=True,n_iter_no_change=100,max_iter=500000,random_state=123)
-base_clf=LogisticRegression(penalty='elasticnet',max_iter=500000,random_state=289,solver='saga',class_weight='balanced')
+base_clf=LogisticRegression(
+    # penalty='elasticnet',
+    max_iter=500000,
+    random_state=56,
+    # solver='saga',
+    class_weight='balanced')
 # base_clf=SVC(probability=True,class_weight="balanced")
 
 
@@ -59,7 +64,7 @@ base_clf=LogisticRegression(penalty='elasticnet',max_iter=500000,random_state=28
 
 grid_parameters = {
     'clf__C': [1.0,5e-1,1e-1,5e-2,1e-2,1e-3,1e-4],
-    'clf__l1_ratio': [0.0, 0.25, 0.5, 0.75, 1.0],
+    # 'clf__l1_ratio': [0.0, 0.25, 0.5, 0.75, 1.0],
 
     # 'clf__C': [1.0, 10, 100, 1000, 10000],
     # 'clf__kernel': ['linear', 'poly', 'rbf'],
@@ -84,7 +89,7 @@ pipeline = Pipeline([
 ])
 
 clf = GridSearchCV(pipeline, param_grid=grid_parameters, cv=StratifiedKFold(10 ),
-                   verbose=3, n_jobs=cores,#n_iter=500,
+                   verbose=1, n_jobs=cores,#n_iter=500,
                    scoring=[ 'balanced_accuracy','roc_auc','f1', 'recall', 'precision'], refit='roc_auc',
                    return_train_score=True,
                    )
