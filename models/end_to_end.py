68a0759f1c364c82f2adbcfe65696c87f09d20e7
Paul Mwaniki <pmmwaniki06@gmail.com>
2021-04-19 11:05:59 +0300

    Added sample weights and save result to db functionality

--- models/end_to_end.py
+++ models/end_to_end.py
@@ -6,8 +6,7 @@ from torch.utils.data import DataLoader,WeightedRandomSampler,random_split
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchsummary import summary
 # import torchaudio
-from settings import data_dir
-from settings import checkpoint_dir as log_dir
+
 import os
 import json
 from models.networks import init_fun
@@ -30,7 +29,9 @@ from datasets.loaders import TriageDataset,TriagePairs
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from tqdm import tqdm
-from settings import Fs,weights_dir
+from settings import data_dir,weights_dir
+from settings import checkpoint_dir as log_dir
+from utils import save_table3
 
 
 import ray
@@ -50,6 +51,7 @@ display=os.environ.get('DISPLAY',None) is not None
 
 enc_representation_size="32"
 res_type="original" # wide,original
+init="random"
 # enc_distance="DotProduct" #LpDistance Dotproduct Cosine
 # distance_fun="euclidean" if enc_distance=="LpDistance" else cosine
 experiment=f"Supervised-{res_type}-{enc_representation_size}"
@@ -79,15 +81,15 @@ triage_segments['label']=pd.factorize(triage_segments['id'])[0]
 
 train_ids,test_ids=joblib.load(os.path.join(data_dir,"triage/ids.joblib"))
 np.random.seed(123)
-val_ids=np.random.choice(train_ids,size=165,replace=False)
+# val_ids=np.random.choice(train_ids,size=165,replace=False)
 #
 #
-train=triage_segments[triage_segments['id'].isin(train_ids)]
+train=triage_segments.loc[triage_segments['id'].isin(train_ids),:]
 # val=train[train['id'].isin(val_ids)]
 # train=train[~train['id'].isin(val_ids)]
 
 
-test=triage_segments[triage_segments['id'].isin(test_ids)]
+test=triage_segments.loc[triage_segments['id'].isin(test_ids),:]
 
 
 # encoder_test_dataset = TriagePairs(test_encoder, id_var="id", stft_fun=None, aug_raw=[],normalize=True)
@@ -147,12 +149,7 @@ test=triage_segments[triage_segments['id'].isin(test_ids)]
 
 
 
-# def accuracy_fun(xis_embeddings, xjs_embeddings, distance="minkowski"):
-#     knn = KNeighborsClassifier(n_neighbors=1,metric=distance)
-#     knn.fit(xis_embeddings, np.arange(xis_embeddings.shape[0]))
-#     knn_pred = knn.predict(xjs_embeddings)
-#     accuracy = np.mean(np.arange(xis_embeddings.shape[0]) == knn_pred)
-#     return accuracy
+
 
 def get_loader(config):
     aug_raw=[
@@ -164,22 +161,29 @@ def get_loader(config):
     train_ids_ = np.random.choice(all_ids, size=int(len(all_ids) * 0.85), replace=False)
     train_csv = train[train['id'].isin(train_ids_)]
     val_csv = train[~train['id'].isin(train_ids_)]
-    train_ds=TriageDataset(train_csv,labels='admitted', stft_fun=None,
-                                        transforms=None,
-                                        aug_raw=aug_raw,
-                                        normalize=True
-                                        )
-    val_ds=TriageDataset(val_csv, labels='admitted', stft_fun=None,
-                                        transforms=None,
-                                        aug_raw=[],
-                                        normalize=True
-                                        )
+    train_ds = TriageDataset(train_csv, labels='admitted', stft_fun=None,
+                             transforms=None,
+                             aug_raw=aug_raw,
+                             normalize=True,
+                             sample_by="id"
+                             )
+    val_ds = TriageDataset(val_csv, labels='admitted', stft_fun=None,
+                           transforms=None,
+                           aug_raw=[],
+                           normalize=True,
+                           sample_by='id'
+                           )
 
     # sample weights
+    train_labels=[]
+    for i in range(len(train_ds)):
+        _,lab=train_ds.__getitem__(i)
+        train_labels.append(lab.numpy()[0])
+
     class_sample_count = np.array(
-        [len(np.where(train_csv['admitted'] == t)[0]) for t in np.unique(train_csv['admitted'])])
+        [len(np.where(train_labels == t)[0]) for t in np.unique(train_labels)])
     weight = 1. / class_sample_count
-    samples_weight = np.array([weight[int(t)] for t in train_csv['admitted']])
+    samples_weight = np.array([weight[int(t)] for t in train_labels])
 
     samples_weight = torch.from_numpy(samples_weight)
     samples_weight = samples_weight.double()
@@ -206,9 +210,12 @@ def get_model(config):
     return model
 
 def get_optimizer(config,model):
-    optimizer = torch.optim.Adam(params=model.parameters(),
-                                 lr=config['enc_lr'],
-                                 weight_decay=config['enc_l2'],betas=(0.99,0.999))
+    optimizer = torch.optim.SGD(params=[
+        {'params': model.base_model.parameters()},
+        {'params': model.fc0.parameters(), 'lr': config['lr_fc'], 'weight_decay': config['l2_fc']},
+        {'params': model.fc.parameters(), 'lr': config['lr_fc'], 'weight_decay': config['l2_fc']}
+    ], lr=config['lr'], weight_decay=config['l2'],momentum=0.99)
+
     return optimizer
 
 
@@ -248,7 +255,7 @@ def train_fun(model,optimizer,criterion,device,train_loader,val_loader,scheduler
             val_loss += loss.item() / len(val_loader)
             pred_val.append(logits.sigmoid().squeeze().cpu().numpy().reshape(-1))
             obs_val.append(batch_y.squeeze().cpu().numpy().reshape(-1))
-    scheduler.step(val_loss)
+    if scheduler: scheduler.step()
     pred_val = np.concatenate(pred_val)
     obs_val = np.concatenate(obs_val)
     f1 = f1_score(obs_val,(pred_val>0.5)*1.0)
@@ -262,7 +269,7 @@ class Trainer(tune.Trainable):
         self.optimizer=get_optimizer(config,self.model)
 
         self.criterion=nn.BCEWithLogitsLoss(pos_weight=torch.tensor(config['pos_weight'])).to(device)
-        self.scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,factor=0.5,patience=50,min_lr=1e-6)
+        self.scheduler=torch.optim.lr_scheduler.StepLR(self.optimizer,step_size=100,gamma=0.5)
         self.train_loader,self.val_loader=get_loader(config)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -286,9 +293,11 @@ configs = {
     'dropout':tune.loguniform(0.01,0.5),
     'representation_size':tune.choice([32,]),
     'batch_size':tune.choice([8,16,32,64,128]),
-    'pos_weight':tune.choice([1.0,3.0,5.0,10.0]),
-    'enc_lr':tune.loguniform(0.00001,0.1),
-    'enc_l2':tune.loguniform(0.00001,0.5),
+    'pos_weight':tune.choice([1.0,]),
+    'lr':tune.loguniform(0.00001,0.1),
+    'l2':tune.loguniform(0.000001,0.5),
+    'lr_fc':tune.loguniform(0.00001,0.5),
+    'l2_fc':tune.loguniform(0.000001,1.0),
     'aug_gaus':tune.choice([0,0.2,0.5,0.8,1.0]),
     'aug_num_seg':tune.choice([2,5,10,20,40,80]),
     'aug_prop_seg':tune.choice([0.05,0.1,0.3,0.5,0.9]),
@@ -297,10 +306,10 @@ configs = {
 config={i:v.sample() for i,v in configs.items()}
 
 scheduler = ASHAScheduler(
-        metric="f1",
-        mode="max",
-        max_t=350,
-        grace_period=10,
+        metric="loss",
+        mode="min",
+        max_t=700,
+        grace_period=50,
         reduction_factor=2)
 # scheduler=AsyncHyperBandScheduler(
 #         time_attr="training_iteration",
@@ -310,63 +319,123 @@ scheduler = ASHAScheduler(
 #         max_t=700)
 # scheduler = HyperBandScheduler(metric="loss", mode="min")
 
-if __name__=="__main__":
 
-    reporter = CLIReporter(
-        # parameter_columns=["l1", "l2", "lr", "batch_size"],
-        metric_columns=["loss", "accuracy","f1", "training_iteration"])
-    result = tune.run(
-        Trainer,
-        # metric='loss',
-        # mode='min',
-        checkpoint_at_end=True,
-        resources_per_trial={"cpu": 3, "gpu": 0.3},
-        config=configs,
-        local_dir=os.path.join(log_dir, "Supervised"),
-        num_samples=200,
-        name=experiment,
-        resume=False,
-        scheduler=scheduler,
-        progress_reporter=reporter,
-        reuse_actors=False,
-        raise_on_failed_trial=False)
-    df = result.results_df
-    # df.to_csv(os.path.join(data_dir, "results/hypersearch.csv"), index=False)
-    best_trial = result.get_best_trial("f1", "max", "last")
-    best_config=result.get_best_config('f1','max')
-
-    best_model=get_model(best_config)
-    best_trial=result.get_best_trial('f1','max')
-    best_checkpoint=result.get_best_checkpoint(best_trial,'f1','max')
-    model_state=torch.load(best_checkpoint)
+reporter = CLIReporter(
+    # parameter_columns=["l1", "l2", "lr", "batch_size"],
+    metric_columns=["loss", "accuracy","f1", "training_iteration"])
+result = tune.run(
+    Trainer,
+    # metric='loss',
+    # mode='min',
+    checkpoint_at_end=True,
+    resources_per_trial={"cpu": 4, "gpu": 0.3},
+    config=configs,
+    local_dir=os.path.join(log_dir, "Supervised"),
+    num_samples=200,
+    name=experiment,
+    resume=False,
+    scheduler=scheduler,
+    progress_reporter=reporter,
+    reuse_actors=False,
+    raise_on_failed_trial=False)
+df = result.results_df
+# df.to_csv(os.path.join(data_dir, "results/hypersearch.csv"), index=False)
+best_trial = result.get_best_trial("auc", "max", "last")
+best_config=result.get_best_config('auc','max')
+best_trainer=Trainer(best_config)
 
 
-    best_model.load_state_dict(model_state)
-    best_model.to(device)
-    # Test model accuracy
-    best_model.eval()
-    pred_test=[]
-    test_dataset=TriageDataset(test,labels='admitted')
-    test_loader=DataLoader(test_dataset,batch_size=16,num_workers=5)
-    criterion=nn.BCEWithLogitsLoss(pos_weight=torch.tensor(1.5)).to(device)
-    best_model.eval()
-    test_loss = 0
-    pred_test=[]
-    obs_test=[]
-    with torch.no_grad():
-        for batch_x,batch_y in test_loader:
-            batch_x, batch_y = batch_x.to(device, dtype=torch.float), batch_y.to(device)
-            logits = best_model(batch_x)
+aug_raw=[
+    partial(gaus_noise,min_sd=1e-5,max_sd=1e-1,p=best_config['aug_gaus']),
+    partial(permute,n_segments=best_config['aug_num_seg'],p=best_config['aug_prop_seg']),
+    ]
+train_dataset = TriageDataset(train, labels='admitted', stft_fun=None,
+                         transforms=None,
+                         aug_raw=aug_raw,
+                         normalize=True,
+                         sample_by="id"
+                         )
 
-            loss = criterion(logits, batch_y)
 
-            test_loss += loss.item() / len(test_loader)
-            pred_test.append(logits.sigmoid().squeeze().cpu().numpy().reshape(-1))
-            obs_test.append(batch_y.squeeze().cpu().numpy().reshape(-1))
-    pred_test = np.concatenate(pred_test)
-    obs_test = np.concatenate(obs_test)
-    f1 = f1_score(obs_test,(pred_test>0.5)*1.0)
-    auc=roc_auc_score(obs_test,pred_test)
+# sample weights
+train_labels=[]
+for i in range(len(train_dataset)):
+    _,lab=train_dataset.__getitem__(i)
+    train_labels.append(lab.numpy()[0])
+
+class_sample_count = np.array(
+    [len(np.where(train_labels == t)[0]) for t in np.unique(train_labels)])
+weight = 1. / class_sample_count
+samples_weight = np.array([weight[int(t)] for t in train_labels])
+
+samples_weight = torch.from_numpy(samples_weight)
+samples_weight = samples_weight.double()
+sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
+
+train_loader = DataLoader(train_dataset,
+                          batch_size=int(best_config["batch_size"]),
+                          shuffle=False,sampler=sampler, num_workers=15)
+
+test_dataset=TriageDataset(test, labels='admitted', stft_fun=None,
+                           transforms=None,
+                           aug_raw=[],
+                           normalize=True,
+                           sample_by=None
+                           )
+test_loader=DataLoader(test_dataset,shuffle=False,batch_size=16,num_workers=15)
+
+best_trainer.train_loader=train_loader
+best_trainer.val_loader=test_loader
+
+
+metrics=[]
+for epoch in range(700):
+    r=best_trainer.step()
+    print(f'Epoch {epoch} | train loss {r["train_loss"]:.3f} | loss {r["loss"]:.3f} | auc {r["auc"]:.2f}')
+    # metrics.append((train_loss,val_loss,f1,auc))
+
+best_model=best_trainer.model
+
+
+best_model.to(device)
+# Test model accuracy
+
+
+# criterion=nn.BCEWithLogitsLoss(pos_weight=torch.tensor(1.5)).to(device)
+best_model.eval()
+# test_loss = 0
+pred_test=[]
+obs_test=[]
+with torch.no_grad():
+    for batch_x,batch_y in test_loader:
+        batch_x, batch_y = batch_x.to(device, dtype=torch.float), batch_y.to(device)
+        logits = best_model(batch_x)
+        # loss = criterion(logits, batch_y)
+        # test_loss += loss.item() / len(test_loader)
+        pred_test.append(logits.sigmoid().squeeze().cpu().numpy().reshape(-1))
+        obs_test.append(batch_y.squeeze().cpu().numpy().reshape(-1))
+
+pred_test = np.concatenate(pred_test)
+obs_test = np.concatenate(obs_test)
+
+test_d=test.copy()
+test_d['pred']=pred_test
+test_d2=test_d.groupby(id)[['admitted','pred']].mean()
+
+f1 = f1_score(test_d2['admitted'],(test_d2['pred']>0.5)*1.0)
+auc=roc_auc_score(test_d2['admitted'],test_d2['pred'])
+report=classification_report(test_d2['admitted'],(test_d2['pred']>0.5)*1.0,output_dict=True)
+recall=report['1.0']['recall']
+precision=report['1.0']['precision']
+f1=report['1.0']['f1-score']
+specificity=report['0.0']['recall']
+acc=report['accuracy']
+
+torch.save(best_model.state_dict(),os.path.join(weights_dir,"end to end.pth"))
+
+save_table3(model="End to end",precision=precision,recall=recall,specificity=specificity,auc=auc,
+            details=json.dumps({'init':'random','exp':experiment}),
+            other=json.dumps({'host':os.uname()[1],'config':best_config}))
 
 
 
