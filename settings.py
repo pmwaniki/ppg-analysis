b61c0fdfef54ddff091c458721346a21f192123a
Paul Mwaniki <pmmwaniki06@gmail.com>
2022-09-17 14:15:42 +0300

    added list of possible clinical predictors

--- settings.py
+++ settings.py
@@ -18,3 +18,19 @@ output_dir=os.path.join(data_dir,"results/outputs")
 
 segment_length=10
 segment_slide=2
+
+clinical_predictors=[
+'Weight-Height Z score',
+'MUAC (Mid-upper arm circumference) cm',
+       'Temperature (degrees celsius)',
+       'Respiratory rate- RR (per minute)', 'Oxygen saturation',
+       'Heart rate(HR) ', 'Length of illness (days)', 'Fever','Cough',
+'Difficulty breathing', 'Diarrhoea','Vomiting ','Difficulty feeding',
+'Convulsions','Jaundice', 'Visible severe wasting', 'Stridor',
+       'Obstructed breathing ', 'Absent breathing(apnoea)',
+       'Central cyanosis', 'Indrawing', 'Grunting', 'Acidotic breathing',
+       'Wheeze', 'Flaring of alae nasi', 'Head nodding/bobbing',
+       'Peripheral Pulse', 'Cap refill', 'Pallor / Anaemia',
+
+
+]
